f206b29c50427147ff7e4fd1d25ed85796c07b96
Vasil <vasil.todorov@student.fontys.nl>
2022-10-11 11:53:24 +0200

    add post request function and list books in browser

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
 const { connectToDb, getDb} = require("./db")
 
 // init app & middleware
 const app = express()
+app.use(express.json())
 
 // db connection
 let db
@@ -18,5 +20,47 @@ connectToDb((err) => {
 
 // routes
 app.get("/books", (req,res) => {
-    res.json({mssg: "welcome to the api"})
+    let books = []
+
+    db.collection("books")
+        .find()
+        .sort({ author: 1 })
+        .forEach(book => books.push(book))
+        .then(() => {
+            res.status(200).json(books)
+        })
+        .catch(() => {
+            res.status(500).json({error: "Could not fetch documents"})
+        })
+})
+
+app.get("/books/:id", (req, res) => {
+
+    if(ObjectId.isValid(req.params.id)) {
+            db.collection("books")
+              .findOne({ _id: ObjectId(req.params.id) })
+              .then((doc) => {
+                res.status(200).json(doc);
+              })
+              .catch((err) => {
+                res
+                  .status(500)
+                  .json({ error: "Could not fetch the documents" });
+              });
+    } else {
+        res.status(500).json({error: "Not a valid doc id"})
+    }
+})
+
+app.post('/books', (req, res) => {
+    const book = req.body
+
+    db.collection("books")
+        .insertOne(book)
+        .then(result => {
+            res.status(201).json(result)
+        })
+        .catch(err => {
+            res.status(500).json({err: "Could not create a new  document"})
+        })
 })
