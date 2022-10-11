72e9fa89949d300feffb6a0ff74701795658d3b5
Vasil <vasil.todorov@student.fontys.nl>
2022-10-11 12:23:35 +0200

    add patch request function

--- app.js
+++ app.js
@@ -81,3 +81,22 @@ app.delete("/books/:id", (req, res) => {
     }
 
 })
+
+app.patch("/books/:id", (req, res) => {
+    const updates = req.body
+
+    if (ObjectId.isValid(req.params.id)) {
+      db.collection("books")
+        .updateOne({ _id: ObjectId(req.params.id)}, {$set: updates})
+        .then((result) => {
+          res.status(200).json(result);
+        })
+        .catch((err) => {
+          res.status(500).json({ error: "Could not update the documents" });
+        });
+    } else {
+      res.status(500).json({ error: "Not a valid doc id" });
+    }
+
+
+})
