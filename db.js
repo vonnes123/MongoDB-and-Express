5853d48ec1551a2850ee564672273fabe739baef
Vasil <vasil.todorov@student.fontys.nl>
2022-10-11 12:54:59 +0200

    add link to mongodb atlas

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const { MongoClient } = require('mongodb')
 
 let dbConnection
+let uri = "mongodb+srv://vasil:test123@cluster0.gx8vpzc.mongodb.net/?retryWrites=true&w=majority";
 
 module.exports = {
     connectToDb: (cb) => {
-        MongoClient.connect('mongodb://localhost:27017/bookstore')
+        MongoClient.connect(uri)
             .then((client) => {
                 dbConnection = client.db()
                 return cb()
